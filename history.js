ecc24673df4cd083c555b93fe08a5e490d3a834c
sajid365-sr <sajidsorker86@gmail.com>
2022-09-14 23:31:05 +0600

    modal and history section seconds reset to intger

--- history.js
+++ history.js
@@ -3,7 +3,6 @@ const histories = document.getElementById("histories");
 function addHistory(questionText, timeTaken, errorCount) {
   const newRow = document.createElement("div");
   newRow.classList.add("card");
-
   newRow.innerHTML = `
   <h3>${questionText}</h3>
   <div>
@@ -31,7 +30,7 @@ function displayHistory() {
 
     newRow.innerHTML = `
   <h3>${test.questionText}</h3>
-  <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
+  <p>You took: <span class="bold">${test.timeTaken.toFixed(0)}</span> seconds</p>
     <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
   `;
 
